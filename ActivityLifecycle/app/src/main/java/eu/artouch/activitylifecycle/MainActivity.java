c86e9212be109062a21d45c715a6829f19bab09b
noway0032@gmail.com <noway0032@gmail.com>
2018-08-16 14:49:32 +0200

    Lifecycle

--- ActivityLifecycle/app/src/main/java/eu/artouch/activitylifecycle/MainActivity.java
+++ ActivityLifecycle/app/src/main/java/eu/artouch/activitylifecycle/MainActivity.java
@@ -2,6 +2,7 @@ package eu.artouch.activitylifecycle;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,48 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Lifecycle", "onCreate called" );
+    }
+
+    /**
+     * Meghívódik ha láthatóvá válik az aktiviti
+     */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("Lifecycle", "onStart called" );
+    }
+
+    /**
+     * Ha fókuszba kerül az aktiviti
+     */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("Lifecycle", "onResume called" );
+    }
+
+    /**
+     * Ha háttérbe kerül az aktiviti
+     */
+    @Override
+    protected void onPause() {
+        Log.d("Lifecycle", "onPause called" );
+        super.onPause();
+    }
+
+    /**
+     * Meghívódik ha már nem látható
+     */
+    @Override
+    protected void onStop() {
+        Log.d("Lifecycle", "onStop called" );
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("Lifecycle", "onDestroy called" );
+        super.onDestroy();
     }
 }
