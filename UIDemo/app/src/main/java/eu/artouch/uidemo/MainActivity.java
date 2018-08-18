9ff73b99075760af80921951c7ff306115236073
noway0032@gmail.com <noway0032@gmail.com>
2018-08-18 21:58:43 +0200

    UI start

--- UIDemo/app/src/main/java/eu/artouch/uidemo/MainActivity.java
+++ UIDemo/app/src/main/java/eu/artouch/uidemo/MainActivity.java
@@ -1,7 +1,8 @@
 package eu.artouch.uidemo;
 
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+        import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Lifecycle", "onCreate called");
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("Lifecycle", "onDestroy called");
+        super.onDestroy();
     }
 }
