a2dc37ba7334b8daf253434f0566e7689b074508
noway0032@gmail.com <noway0032@gmail.com>
2018-08-19 23:47:13 +0200

    Views

--- UIDemo/app/src/main/java/eu/artouch/uidemo/MainActivity.java
+++ UIDemo/app/src/main/java/eu/artouch/uidemo/MainActivity.java
@@ -1,8 +1,11 @@
 package eu.artouch.uidemo;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
         import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +14,15 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.d("Lifecycle", "onCreate called");
+
+        Button navigationButton = findViewById(R.id.navigationButton);
+        navigationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, RelativeActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 
     @Override
