22e65eceb835baceaa68285a6e2793ecbf15f0bd
noway0032@gmail.com <noway0032@gmail.com>
2018-08-21 00:53:45 +0200

    LinearLayoutManager

--- TodoApp/app/src/main/java/eu/artouch/todoapp/TodoActivity.java
+++ TodoApp/app/src/main/java/eu/artouch/todoapp/TodoActivity.java
@@ -2,24 +2,67 @@ package eu.artouch.todoapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import eu.artouch.todoapp.adapter.TodoAdapter;
 import eu.artouch.todoapp.model.Todo;
 
 public class TodoActivity extends AppCompatActivity {
+
+    private List<Todo> todos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_todo);
-        List<Todo> todos = new ArrayList<>();
-        todos.add(new Todo("Karácsonyi ajándékok", "Megvenni", "Én"));
-        todos.add(new Todo("Előadás", "Megtartani", "Péter"));
-        todos.add(new Todo("Virágok", "Meglocsolni", "Kertész"));
-        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        todos = new ArrayList<>();
+        todos.add(new Todo("01. Karácsonyi ajándékok", "Megvenni", "Én"));
+        todos.add(new Todo("02. Előadás", "Megtartani", "Péter"));
+        todos.add(new Todo("03. Virágok", "Meglocsolni", "Kertész"));
+        todos.add(new Todo("04. Karácsonyi ajándékok", "Megvenni", "Én"));
+        todos.add(new Todo("05. Előadás", "Megtartani", "Péter"));
+        todos.add(new Todo("06. Virágok", "Meglocsolni", "Kertész"));
+        todos.add(new Todo("07. Karácsonyi ajándékok", "Megvenni", "Én"));
+        todos.add(new Todo("08. Előadás", "Megtartani", "Péter"));
+        todos.add(new Todo("09. Virágok", "Meglocsolni", "Kertész"));
+        todos.add(new Todo("10. Karácsonyi ajándékok", "Megvenni", "Én"));
+        todos.add(new Todo("11. Előadás", "Megtartani", "Péter"));
+        todos.add(new Todo("12. Virágok", "Meglocsolni", "Kertész"));
+        todos.add(new Todo("13. Karácsonyi ajándékok", "Megvenni", "Én"));
+        todos.add(new Todo("14. Előadás", "Megtartani", "Péter"));
+        todos.add(new Todo("15. Virágok", "Meglocsolni", "Kertész"));
+        todos.add(new Todo("16. Karácsonyi ajándékok", "Megvenni", "Én"));
+        todos.add(new Todo("17. Előadás", "Megtartani", "Péter"));
+        todos.add(new Todo("18. Virágok", "Meglocsolni", "Kertész"));
+        todos.add(new Todo("19. Karácsonyi ajándékok", "Megvenni", "Én"));
+        todos.add(new Todo("20. Előadás", "Megtartani", "Péter"));
+        todos.add(new Todo("21. Virágok", "Meglocsolni", "Kertész"));
+        todos.add(new Todo("22. Karácsonyi ajándékok", "Megvenni", "Én"));
+        todos.add(new Todo("23. Előadás", "Megtartani", "Péter"));
+        todos.add(new Todo("24. Virágok", "Meglocsolni", "Kertész"));
+        todos.add(new Todo("25. Karácsonyi ajándékok", "Megvenni", "Én"));
+        todos.add(new Todo("26. Előadás", "Megtartani", "Péter"));
+        todos.add(new Todo("27. Virágok", "Meglocsolni", "Kertész"));
+        todos.add(new Todo("28. Karácsonyi ajándékok", "Megvenni", "Én"));
+        todos.add(new Todo("29. Előadás", "Megtartani", "Péter"));
+        todos.add(new Todo("30. Virágok", "Meglocsolni", "Kertész"));
+        todos.add(new Todo("31. Karácsonyi ajándékok", "Megvenni", "Én"));
+        todos.add(new Todo("32. Előadás", "Megtartani", "Péter"));
+        todos.add(new Todo("33. Virágok", "Meglocsolni", "Kertész"));
+        todos.add(new Todo("34. Karácsonyi ajándékok", "Megvenni", "Én"));
+        todos.add(new Todo("35. Előadás", "Megtartani", "Péter"));
+        todos.add(new Todo("36. Virágok", "Meglocsolni", "Kertész"));
 
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        TodoAdapter adapter = new TodoAdapter(this, todos);
+
+        RecyclerView recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(adapter);
     }
 }
--- TodoApp/app/src/main/java/eu/artouch/todoapp/adapter/TodoAdapter.java
+++ TodoApp/app/src/main/java/eu/artouch/todoapp/adapter/TodoAdapter.java
@@ -2,6 +2,7 @@ package eu.artouch.todoapp.adapter;
 
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,25 +25,47 @@ public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.ViewHolder> {
         this.inflater=LayoutInflater.from(context);
     }
 
+    /**
+     * Meghívja ha létre kell hozni za egyes felületi elemeket
+     * @param parent
+     * @param viewType Megadható, hogy miyen típusu elemeket használjon
+     * @return
+     */
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        LayoutInflater.from(context);
-        return null;
+        View view = inflater.inflate(R.layout.li_todo,parent,false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
     }
 
+    /**
+     * Meghívja ha Újra-Használja az elemet és adatokat tölt bele
+     * A lehető legkevesebb hívást kell bele tenni, ha "megrántjuk" a legörgetést több 10szer lefuthat
+     * @param holder
+     * @param position
+     */
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+        Todo todo = todos.get(position);
+        holder.titleTV.setText(todo.getTitlr());
+        holder.assigneeTV.setText("Felelős: "+todo.getAssignee());
+        holder.descriptionTV.setText(todo.getDescription());
+        Log.d("ViewHolder", "Id: " + position);
     }
 
+    /**
+     * A lista összes elemének darabszáma
+     * @return
+     */
     @Override
     public int getItemCount() {
-        return 0;
+        return todos.size();
     }
 
+    /**
+     * Ezzel az osztályal tudja megoldani a nézetek újra használatát
+     */
     public static class ViewHolder extends RecyclerView.ViewHolder {
-
-
         public final TextView titleTV;
         public final TextView descriptionTV;
         public final TextView assigneeTV;
