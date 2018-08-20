fb9701a27489b2d927a547d3f2fb45ca9d5bb5f5
noway0032@gmail.com <noway0032@gmail.com>
2018-08-20 18:40:18 +0200

    TodoAdapter

--- TodoApp/app/src/main/java/eu/artouch/todoapp/adapter/TodoAdapter.java
+++ TodoApp/app/src/main/java/eu/artouch/todoapp/adapter/TodoAdapter.java
@@ -1,13 +1,32 @@
 package eu.artouch.todoapp.adapter;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.List;
+
+import eu.artouch.todoapp.R;
+import eu.artouch.todoapp.model.Todo;
 
 public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.ViewHolder> {
 
+    private final Context context;
+    private final List<Todo> todos;
+    private final LayoutInflater inflater;
+
+    public TodoAdapter(Context context, List<Todo> todos) {
+        this.context = context;
+        this.todos = todos;
+        this.inflater=LayoutInflater.from(context);
+    }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        LayoutInflater.from(context);
         return null;
     }
 
@@ -21,10 +40,18 @@ public class TodoAdapter extends RecyclerView.Adapter<TodoAdapter.ViewHolder> {
         return 0;
     }
 
-    public static class ViewHolder extends RecyclerView.ViewHolder{
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+
+
+        public final TextView titleTV;
+        public final TextView descriptionTV;
+        public final TextView assigneeTV;
 
         public ViewHolder(View itemView) {
             super(itemView);
+            titleTV = itemView.findViewById(R.id.titleTV);
+            descriptionTV = itemView.findViewById(R.id.descriptionTV);
+            assigneeTV = itemView.findViewById(R.id.assigneeTV);
         }
     }
 }
